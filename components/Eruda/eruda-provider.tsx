ef543e1a7e9c477eae8f958630b86f309eebce17
Wallet-bussines <pasarelawld@gmail.com>
2025-01-24 18:13:47 -0500

    Update eruda-provider.tsx

--- components/Eruda/eruda-provider.tsx
+++ components/Eruda/eruda-provider.tsx
@@ -1,11 +1,17 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { ReactNode, useEffect } from "react";
+// Importación dinámica del componente Eruda
+const Eruda = dynamic(() => import("../components/Eruda"), {
+  ssr: false, // Evitar la renderización del lado del servidor
+});
 
-export const Eruda = (props: { children: ReactNode }) => {
-  useEffect(() => {
-    // No inicializar Eruda ni ejecutar ningún código relacionado
-  }, []);
-
-  return <>{props.children}</>;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Eruda>
+      {/* Aquí se renderiza el contenido de la página */}
+      <div>{children}</div>
+    </Eruda>
+  );
 };
+
+export default Layout;
