fc28fd7a25a970dc6896d15991c5018c0163cce9
Wallet-bussines <pasarelawld@gmail.com>
2025-01-24 18:40:29 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MiniKitProvider from "@/components/minikit-provider";
-import NextAuthProvider from "@/components/next-auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
