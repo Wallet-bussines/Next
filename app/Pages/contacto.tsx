c9f3028cd2fd0e61d5beb8de74b33c3f7cd51cfe
Wallet-bussines <pasarelawld@gmail.com>
2025-01-24 19:31:43 -0500

    Create contacto.tsx

---
+++ app/Pages/contacto.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contacto: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Contacto</h1>
+      <p>Ponte en contacto con nosotros.</p>
+    </div>
+  );
+};
+
+export default Contacto;
