0784fdc6e8d4f4ea01bbea718f2cbba61d77f104
Wallet-bussines <pasarelawld@gmail.com>
2025-01-24 19:35:17 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import React from "react";
+import Link from "next/link"; // Usamos Link de Next.js para manejar la navegación
 import { PayBlock } from "@/components/Pay";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Contenido principal */}
@@ -10,18 +12,20 @@ export default function Home() {
 
       {/* Barra de navegación fija en la parte inferior */}
       <nav className="bg-blue-600 text-white p-4 fixed bottom-0 w-full z-10">
-        <div className="flex justify-center items-center space-x-10">
-          <a href="/inicio" className="hover:text-blue-300">
-            Inicio
-          </a>
-          <a href="/servicios" className="hover:text-blue-300">
-            Servicios
-          </a>
-          <a href="/contacto" className="hover:text-blue-300">
-            Contacto
-          </a>
+        <div className="flex justify-around items-center">
+          <Link href="/inicio">
+            <a className="hover:text-blue-300">Inicio</a>
+          </Link>
+          <Link href="/servicios">
+            <a className="hover:text-blue-300">Servicios</a>
+          </Link>
+          <Link href="/contacto">
+            <a className="hover:text-blue-300">Contacto</a>
+          </Link>
         </div>
       </nav>
     </div>
   );
-}
+};
+
+export default Home;
