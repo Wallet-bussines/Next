0717cd44a62d44bd335f493d834643fc44cd8061
Wallet-bussines <pasarelawld@gmail.com>
2025-01-24 19:30:22 -0500

    Create inicio.tsx

---
+++ app/Pages/inicio.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Inicio: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Inicio</h1>
+      <p>Bienvenido a la página de inicio.</p>
+    </div>
+  );
+};
+
+export default Inicio;
